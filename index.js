8f117ef52bbf3cbbf360776cd75501148f66ae6d
Eric <eric.keith@flatironschool.com>
2023-02-02 15:51:09 -0600

    commit tree bbaa965978d8d9735947825431692a7d06c83049

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const messages = [];
+function writeCards(message, message2) {
+	for (let i = 0; i < message.length; i++ ) {
+		messages.push(`Thank you, ${message[i]}, for the wonderful ${message2} gift!`);
+	}
+	console.log(messages);
+	return messages;
+};
+
+function countDown(count) {
+	while (count > -1) {
+		console.log(count);
+		count--;
+	}
+}
